8e8a0ed81f6d9f3be434304da12356eb48eee8b2
Matheesha Nayanajith Mahaliyana <nayanajithmatheesha@gmail.com>
2021-08-31 23:14:56 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,146 @@
+# print("Hello World!")
+# print(123)
+# print(2.4)
+# print("Hello..")
+# print(True)
+# print(False)
+# print(2 + 3j)
+
+# print("Hello")
+# print("World")
+
+# print("Hellow \nWorld")
+
+# print("Name\t: Sadunika")
+# print("Moodada\t: Yes")
+
+# print("saman said \"Good Morning\"")
+# print("Special character \\n gives us a new line ") #\ make ignor 
+
+# #a = 10.5
+# #b = 3.3
+# #c= 5
+
+# #print(a + b)
+# #print(a - b)
+# #print(a * b)
+# #print(a / b)
+# #print(a % b)
+# #print(a // b)
+# #print(a ** b)
+
+# #a = "Hello"
+# #b = "World"
+# #c = 5
+# #d = a + " " + b + " " +str(c)
+# #print(a + b)
+# #print(a * c)
+# #print(d)
+
+# a = True
+# b = False
+
+# age = 50
+
+# is_young = age > 18
+# print(is_young)
+
+# #c = a and b
+# #c = a or b
+# #c = a not b
+# #c = a ^ b
+# #print(c)
+
+# #x = 10
+# #
+# #x = x + 5
+# #x += 5
+# #print(x)
+# #
+# #print(x, type(x))
+# #
+# #a, b, c = 10, 20, "Hello"
+# #print(a, b, c)
+# ##end='' when getting input form user, get it same line 
+# #print("Enter Your Name : ",end='')
+# #name = input()
+# #
+# #print("Enter your age : ", end='')
+# #age = input()
+# #
+# #print("Hello", name)
+# #print("You are", age, "years old.")
+# #
+# #my_variable = bool("True")
+# #my_other_variable = False or True
+# #result = my_variable ^ my_other_variable 
+# #print("Result is", result)
+
+# a, b = 10, 24
+# c = a ** 2
+# d = b % 7
+# e = c // d
+# print("Output is", e)
+
+# x = [12,34,56,67]
+# print(x)
+
+# y = x[0]
+
+# print(x)
+# print(y)
+
+
+# x[0] = 569
+
+# print(x)
+
+# x.append(100) #inset data to end of the list
+# x.append(798)
+
+
+# x.insert(2, 500)#insert data to list any thing
+
+# print(x)
+
+# x.remove(67)#remove value 
+# x.pop(2)#remove
+
+# x = [10, 20, 30, 40, 50]
+# y = [0, 9, 8, 7, 6]
+
+# z = x + y#list can only add
+# print(z)
+
+# a = 10 in z#check values
+# print(a)
+
+
+# x={'1000':"colombo", "12500":"panadura"}
+# x['1200']="Moratuwa"
+# y = x['1000']
+# x['cities']=['kaluthara','Malabe']
+# x['cities'] = {1: '6867', 78: "54567"}
+# print(x)
+# print(x.keys())
+# print(x.values())
+# del x[0]
+# del x['cities']
+# x.clear()
+
+# x = {1: 'A',2: 'B'}
+# y = x.get(3, 0)
+# print(y)
+# print(x)
+
+x = {
+    "a": ['Hello', 'Hi', 'Good morning'],
+    "b": ['Bye', "Good Night"],
+    "c": 16
+}
+y = x['a']
+y.append('aayubowan')
+z = x["c"]
+z = 17
+print(x)
+
